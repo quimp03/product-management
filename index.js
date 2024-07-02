b8d0c934d7834fb768f565efa93cbeb5cd488ef1
Mai Phuc Qui <maiphucqui8482@gmail.com>
2024-07-02 15:49:19 +0700

    Kết nối database

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const database = require("./config/database")
 dotenv.config()
 const app = express();
 const port = process.env.PORT;
+const routeClient = require('./routers/client/index.route')
+database.connect()
 app.use(express.static('public'));
 app.set('view engine', 'pug');
 app.set('views', './views');
-const routeClient = require('./routers/client/index.route')
 routeClient(app)
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
