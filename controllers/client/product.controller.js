e4e171954d21ad87f41af9427382a37a5246c900
Mai Phuc Qui <maiphucqui8482@gmail.com>
2024-07-02 22:43:49 +0700

    Làm trang danh sách sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,15 @@
-module.exports.index = (req, res) => {
+const Product = require("../../model/product.model")
+module.exports.index = async(req, res) => {
+    const find = {
+        status: "active",
+        deleted: false
+    }
+    const products = await Product.find(find)
+    for (const item of products) {
+        item.newPrice = (item.price * ((100 - item.discountPercentage)/100)).toFixed(0)
+    }
     res.render("client/pages/products/index.pug", {
-        pageTitle: "Danh sách sản phẩm"
+        pageTitle: "Danh sách sản phẩm",
+        products: products,
     })
 }
