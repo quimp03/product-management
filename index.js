89faa55fd37ee02e8459b7be0ee54687ab4a4905
Mai Phuc Qui <maiphucqui8482@gmail.com>
2024-07-02 15:16:59 +0700

    Fix layout

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 dotenv.config()
 const app = express();
 const port = process.env.PORT;
+app.use(express.static('public'));
 app.set('view engine', 'pug');
 app.set('views', './views');
 const routeClient = require('./routers/client/index.route')
