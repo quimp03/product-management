4b33a028053a8d1829f579b5cec828ae8c71f724
Mai Phuc Qui <maiphucqui8482@gmail.com>
2024-07-04 00:44:47 +0700

    Tính năng lọc theo trạng thái

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -3,9 +3,37 @@ module.exports.index = async(req, res) => {
     const find = {
         deleted: false
     }
+    const filterStatus = [
+        {
+            name: "Tất cả",
+            status: "",
+            class: ""
+        },
+        {
+            name: "Hoạt động",
+            status: "active",
+            class: ""
+        },
+        {
+            name: "Dừng hoạt động",
+            status: "inactive",
+            class: ""
+        }
+    ]
+    if(req.query.status){
+        const index = filterStatus.findIndex(item => item.status == req.query.status)
+        filterStatus[index].class = "active"
+    }else{
+        const index = filterStatus.findIndex(item => item.status == "")
+        filterStatus[index].class = "active"
+    }
+    if(req.query.status){
+        find.status = req.query.status
+    }
     const products = await Product.find(find)
     res.render("admin/pages/product/index.pug", {
         pageTitle: "Danh sách sản phẩm", 
-        products: products
+        products: products,
+        filterStatus: filterStatus,
     })
 }
---
+++ public/admin/js/script.js
@@ -0,0 +1,18 @@
+//filter status
+const listButtonFilterStatus = document.querySelectorAll("[button-status]")
+if(listButtonFilterStatus.length > 0){
+    const url = new URL(window.location.href)
+    console.log(url)
+    listButtonFilterStatus.forEach(button => {
+        button.addEventListener("click", () => {
+            const status = button.getAttribute("button-status")
+           if(status){
+            url.searchParams.set("status",status)
+           }else{
+            url.searchParams.delete("status")
+           }
+           window.location.href = url.href
+        })
+    })
+}
+//end filter status
