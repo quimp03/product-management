4334711919c0d978db97c2c4d85c46fa38c814fa
Mai Phuc Qui <maiphucqui8482@gmail.com>
2024-07-04 23:59:40 +0700

    Fix tính năng tìm kiếm không làm mất bộ lọc

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -15,3 +15,21 @@ if(listButtonFilterStatus.length > 0){
     })
 }
 //end filter status
+// Form Search
+const formSearch = document.querySelector("#form-search");
+if(formSearch) {
+  let url = new URL(window.location.href);
+
+  formSearch.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const keyword = event.target.elements.keyword.value;
+
+    if(keyword) {
+      url.searchParams.set("keyword", keyword);
+    } else {
+      url.searchParams.delete("keyword");
+    }
+    window.location.href = url.href;
+  });
+}
+// End Form Search
