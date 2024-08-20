8770e6f98f05b00977d6a7ecbed67bf9686fb6ba
Mai Phuc Qui <maiphucqui8482@gmail.com>
2024-08-20 18:16:37 +0700

    Xong phần chỉnh sửa danh mục sản phẩm

--- controllers/admin/products-category.controller.js
+++ controllers/admin/products-category.controller.js
@@ -1,13 +1,18 @@
 const ProductCategory = require("../../model/product-category.model")
 const systemConfig = require("../../config/system");
 const createTreeHelper = require("../../helper/createTree.helper");
+const paginationHelper = require("../../helper/pagination.helper")
 module.exports.index = async (req, res) => {
-    const products = await ProductCategory.find({
-        deleted: false,
-    })
+    const find = {
+        deleted: false
+    }
+    const countRecords = await ProductCategory.countDocuments(find)
+    const objectPagination =  paginationHelper(req, countRecords)
+    const products = await ProductCategory.find(find).limit(objectPagination.limitItems).skip(objectPagination.skip)
     res.render("admin/pages/productCategory/index.pug", {
         pageTitle: "Danh mục sản phẩm",
-        products: products
+        products: products,
+        objectPagination: objectPagination
     })
 }
 module.exports.create = async(req, res) => {
@@ -55,14 +60,38 @@ module.exports.deleteProductCategory = async(req, res) => {
     req.flash("success", "Xóa danh mục sản phẩm thành công!")
     res.redirect("back")
 }
-module.exports.eidt = async(req, res) => {
+module.exports.edit = async (req, res) => {
+    try {
+      const find = {
+        _id: req.params.id,
+        deleted: false,
+      };
+      const data = await ProductCategory.findOne(find);
+      const records = await ProductCategory.find({
+        deleted: false,
+      });
+      const newRecords = createTreeHelper(records);
+      res.render("admin/pages/productCategory/edit", {
+        pageTitle: "Chỉnh sửa danh mục sản phẩm",
+        data: data,
+        records: newRecords,
+      });
+    } catch (error) {
+      res.redirect(`/${systemConfig.prefixAdmin}/productCategory`);
+    }
+  };
+module.exports.editPatch = async(req, res) => {
+   try {
     const id = req.params.id
-    const product = await ProductCategory.findOne({
+    req.body.position = parseInt(req.body.position)
+    await ProductCategory.updateOne({
         _id: id,
         deleted: false
-    })
-    res.render(`admin/pages/productCategory/edit`, {
-        pageTitle: "Trang chỉnh sửa",
-        product: product
-    })
+    }, req.body)
+    req.flash("success", "Cập nhật thành công!")
+   } catch (error) {
+    console.log(error)
+    req.flash("error", "Cập nhật không thành công!")
+   }
+   res.redirect("back")
 }
--- routers/admin/products-category.route.js
+++ routers/admin/products-category.route.js
@@ -8,7 +8,8 @@ const controller = require("../../controllers/admin/products-category.controller
 router.get("/", controller.index)
 router.get("/create", controller.create)
 router.post("/create", upload.single('thumbnail'), uploadCloud.uploadSingle, validate.createPost, controller.createPost)
-router.patch("/:status/:id", controller.changeStatus)
+router.patch("/change-status/:status/:id", controller.changeStatus)
 router.delete("/delete/:id", controller.deleteProductCategory)
-router.get("/edit/:id", controller.eidt)
+router.get("/edit/:id", controller.edit)
+router.patch("/edit/:id",upload.single('thumbnail'), uploadCloud.uploadSingle, validate.createPost, controller.editPatch)
 module.exports = router
