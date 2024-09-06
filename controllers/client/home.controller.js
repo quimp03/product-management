093280448f946559e47e2538ec18522407bb171a
Mai Phuc Qui <maiphucqui8482@gmail.com>
2024-09-07 00:57:46 +0700

    Sản phẩm mới nhất bên client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,22 @@
-module.exports.index = (req, res) => {
+const Product = require("../../model/product.model")
+module.exports.index = async(req, res) => {
+    //Sản phẩm nổi bật
+    const productsFeatured = await Product.find({
+        deleted: false,
+        status: "active",
+        featured: "1"
+    })
+    //Sản phẩm mới nhất
+    const productsNew = await Product.find({
+        deleted: false,
+        status: "active",
+    }).sort({position: "desc"}).limit(6).select("-decription")
+    for (const product of productsNew) {
+        product.newPrice = (product.price * (100 - product.discountPercentage)/ 100).toFixed(0)
+    }
     res.render("client/pages/home/index", {
-        pageTitle:"Trang chủ"
+        pageTitle:"Trang chủ",
+        productsFeatured: productsFeatured,
+        productsNew: productsNew
     })
 }
